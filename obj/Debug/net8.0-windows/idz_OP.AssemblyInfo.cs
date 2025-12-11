c8697a1df6b090325171b71fd49fffa078a94b3f
Nesterenko Vladyslav <vladislavnesterenko200711@gmail.com>
2025-12-11 01:46:02 +0100

    Add modified master

--- obj/Debug/net8.0-windows/idz_OP.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/idz_OP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("idz_OP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9a102acf5ad8fdc9dd95e833bbf19711ca20f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("idz_OP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("idz_OP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
